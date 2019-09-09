1365f74a641c326a2f8815eb1f952c2ec6f80f01
chenzeyu <624205157@qq.com>
2019-09-09 17:30:41 +0800

    1

---
+++ app/src/main/java/com/zhhl/ducha/activity/KeypersonnelActivityDetalis.java
@@ -0,0 +1,21 @@
+package com.zhhl.ducha.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.zhhl.ducha.R;
+
+/**
+ * Created by qgl on 2019/9/9 16:19.
+ */
+public class KeypersonnelActivityDetalis extends BaseActivity
+{
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.keypersonnelactivity_detalis);
+
+    }
+}
